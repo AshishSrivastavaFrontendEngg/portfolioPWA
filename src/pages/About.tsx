ade12f962427f409b4bfdf56dd971db6d0ded21d
Ashish Srivastava <ashusrivastava21@gmail.com>
2024-01-09 12:37:43 +0530

    Added Header & routes

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>Your bio or description goes here.</p>
+    </div>
+  );
+};
+
+export default About;
